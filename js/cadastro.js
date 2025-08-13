ceb759d10d5411bed61b2c1a88b65de90fb3397c
Pedro Henrique <santanapedro0105@gmail.com>
2025-08-13 14:10:29 -0300

    mascaraCPF

---
+++ js/cadastro.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const cpfInput = document.getElementById('cpf');
+  if (cpfInput) {
+    cpfInput.addEventListener('input', function(e) {
+      let value = e.target.value.replace(/\D/g, '');
+      value = value.replace(/(\d{3})(\d)/, '$1.$2');
+      value = value.replace(/(\d{3})(\d)/, '$1.$2');
+      value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+      e.target.value = value;
+    });
+  }
+});
